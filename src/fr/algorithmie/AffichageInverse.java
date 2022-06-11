b80537d279895468b29a40ca998747834b4d6ede
boudimohand <boudi_mohand@yahoo.fr>
2022-06-11 21:36:44 +0200

    Creation d'une class Affichage inverse

---
+++ src/fr/algorithmie/AffichageInverse.java
@@ -0,0 +1,22 @@
+package fr.algorithmie;
+
+public class AffichageInverse {
+
+	public AffichageInverse() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//tableau
+		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
+		//affichage de toutes les valeurs du tableau
+		for (int valeurs : array) {
+			System.out.println(valeurs);
+			
+		}
+		
+
+	}
+
+}
