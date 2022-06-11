adea64830227d5802776b0b224eb88a6b45cbbf0
boudimohand <boudi_mohand@yahoo.fr>
2022-06-11 21:10:20 +0200

    Création d'une class DeclarationApp

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,26 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Les variables
+		  byte var1= 10;
+		  short var2=31000;
+		  int var3=2;
+		  long var4=254;
+		  float var5=2.5F;
+		  double var6=3.55;
+		  char var7= 'a';
+		  boolean var8=true;
+		  String randomString = "Voici le résultat d’un calcul :" +"\n"+ "1+5=6";
+		  String var9= "Bonjour tout le monde!!!";
+		  // Affichage des variables
+		  System.out.println("la valeur de la variable var9 est "+ var9);
+		  System.out.println(randomString);
+
+	}
+
+}
