7a1ac421712bcca8affe7303f757e53f8f686e66
boudimohand <boudi_mohand@yahoo.fr>
2022-07-14 21:48:28 +0200

    Modiffication de la classe Affichage inverse

--- src/fr/algorithmie/AffichageInverse.java
+++ src/fr/algorithmie/AffichageInverse.java
@@ -1,22 +1,34 @@
 package fr.algorithmie;
 
+import java.util.Arrays;
+
 public class AffichageInverse {
 
 	public AffichageInverse() {
-		// TODO Auto-generated constructor stub
+	
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//tableau
-		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
-		//affichage de toutes les valeurs du tableau
-		for (int valeurs : array) {
+		
+		//Tableau
+		int[] tab = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
+		
+		//Affichage de toutes les valeurs du tableau
+		for (int valeurs : tab) {
 			System.out.println(valeurs);
 			
 		}
+		//Affichage d'un tableau renversé
+		 for(int i=0; i< tab.length/2; i++){
+		        int tmp = tab[i];
+		        tab[i] = tab[tab.length-i-1];
+		        tab[tab.length-i-1] = tmp;
+		    }
+		    System.out.println("Tableau après l'inversion: " + Arrays.toString(tab));
 		
 
 	}
 
 }
+
+
--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -10,9 +10,9 @@ public class DeclarationApp {
 		  byte var1= 10;
 		  short var2=31000;
 		  int var3=2;
-		  long var4=254;
+		  long var4=254L;
 		  float var5=2.5F;
-		  double var6=3.55;
+		  double var6=3.55D;
 		  char var7= 'a';
 		  boolean var8=true;
 		  String randomString = "Voici le résultat d’un calcul :" +"\n"+ "1+5=6";
